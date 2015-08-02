d7c8de81f1b307d12c96f465f4ef502caac40c0d
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2015-08-02 18:17:35 +0500

    User authentication from preferences

--- app/src/main/java/com/suh/itboy/auctionsystem/Activities/SplashActivity.java
+++ app/src/main/java/com/suh/itboy/auctionsystem/Activities/SplashActivity.java
@@ -2,16 +2,21 @@ package com.suh.itboy.auctionsystem.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.suh.itboy.auctionsystem.Helper.UserSessionHelper;
 import com.suh.itboy.auctionsystem.R;
 
 public class SplashActivity extends Activity {
+    private static final int SPLASH_WAIT_TIME = 1000;
     Runnable runStartAccountActivity;
     Handler handler;
+
+    private boolean isUserLogin = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,41 +26,52 @@ public class SplashActivity extends Activity {
 
         setContentView(R.layout.activity_splash);
 
+        isUserLogin = UserSessionHelper.getInstance(this).isUserLogin();
+
         initializeHandler();
-
-
     }
-    private void initializeHandler(){
+
+    private void initializeHandler() {
         handler = new Handler();
         runStartAccountActivity = new Runnable() {
             @Override
             public void run() {
-                startAccountActivity();
+                startNextActivity();
             }
         };
-
-        handler.postDelayed(runStartAccountActivity, 000);
-
-
+        handler.postDelayed(runStartAccountActivity, SPLASH_WAIT_TIME);
     }
-    private void initialSetup(){
+
+    private void initialSetup() {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         decorView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if (event.getActionMasked() == MotionEvent.ACTION_DOWN){
+                if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
                     handler.removeCallbacks(runStartAccountActivity);
-                    startAccountActivity();
+                    startNextActivity();
                 }
                 return false;
             }
         });
+
     }
-    private void startAccountActivity()
-    {
+    private void startNextActivity(){
+        if (isUserLogin){
+            startHomeActivity();
+        }else{
+            startAccountActivity();
+        }
+    }
+    private void startAccountActivity() {
         Intent intent = new Intent(SplashActivity.this, AccountActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    private void startHomeActivity() {
+        Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
         startActivity(intent);
         finish();
     }
