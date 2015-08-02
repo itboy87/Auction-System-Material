76056d5ee4530742d6546ee68095906e809fd471
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2015-08-02 21:30:27 +0500

    ActivityManager

---
+++ app/src/main/java/com/suh/itboy/auctionsystem/Activities/ActivityManager.java
@@ -0,0 +1,24 @@
+package com.suh.itboy.auctionsystem.Activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by itboy on 8/2/2015.
+ */
+public class ActivityManager {
+    public static void startAccountActivity(Activity parentActivity){
+        startActivity(parentActivity,AccountActivity.class);
+        parentActivity.finish();
+    }
+
+    public static void startHomeActivity(Context context){
+        startActivity(context, HomeActivity.class);
+    }
+
+    private static void startActivity(Context context, Class<? extends Activity> activityClass){
+        Intent intent = new Intent(context,activityClass );
+        context.startActivity(intent);
+    }
+}
