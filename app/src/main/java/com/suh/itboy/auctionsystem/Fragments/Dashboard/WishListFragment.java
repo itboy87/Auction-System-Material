924078450de2c3e101eed1d19962df0103e58832
Sabeeh <mr.hussnain.it@gmail.com>
2015-08-11 09:06:19 -0700

    project updated

--- app/src/main/java/com/suh/itboy/auctionsystem/Fragments/Dashboard/WishListFragment.java
+++ app/src/main/java/com/suh/itboy/auctionsystem/Fragments/Dashboard/WishListFragment.java
@@ -1,7 +1,24 @@
 package com.suh.itboy.auctionsystem.Fragments.Dashboard;
 
-/**
- * Created by itboy on 8/11/2015.
- */
-public class WishListFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.suh.itboy.auctionsystem.R;
+
+public class WishListFragment extends Fragment {
+
+    public WishListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wishlist, container, false);
+    }
+
 }
