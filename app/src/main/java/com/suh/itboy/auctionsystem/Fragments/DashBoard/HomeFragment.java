fd876a56402bb5bac53550632d1d81be2bc927ac
Sabeeh <mr.hussnain.it@gmail.com>
2015-08-11 08:52:55 -0700

    project updated

--- app/src/main/java/com/suh/itboy/auctionsystem/Fragments/DashBoard/HomeFragment.java
+++ app/src/main/java/com/suh/itboy/auctionsystem/Fragments/DashBoard/HomeFragment.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
-import com.suh.itboy.auctionsystem.Adapters.ImageSliderAdapter;
+import com.suh.itboy.auctionsystem.Adapters.ProductGridAdapter;
 import com.suh.itboy.auctionsystem.R;
+import com.suh.itboy.auctionsystem.Utils.App;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,10 +31,10 @@ public class HomeFragment extends Fragment {
         final ViewPager homeViewPager;
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        ViewPager viewPager = (ViewPager)view.findViewById(R.id.slider_viewpager);
-        ImageSliderAdapter imageSliderAdapter = new ImageSliderAdapter(getActivity().getSupportFragmentManager());
+/*        ViewPager viewPager = (ViewPager) view.findViewById(R.id.slider_viewpager);
+        ImageSliderAdapter imageSliderAdapter = new ImageSliderAdapter(getChildFragmentManager());
         viewPager.setAdapter(imageSliderAdapter);
-        homeViewPager = (ViewPager)getActivity().findViewById(R.id.home_viewpager);
+        homeViewPager = (ViewPager) getActivity().findViewById(R.id.home_viewpager);
 
         viewPager.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -40,6 +42,16 @@ public class HomeFragment extends Fragment {
 
                 homeViewPager.requestDisallowInterceptTouchEvent(true);
                 return false;
+            }
+        });*/
+
+        GridView productsGrid = (GridView) view.findViewById(R.id.products);
+        productsGrid.setAdapter(new ProductGridAdapter(getActivity()));
+
+        productsGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                App.ShowMsg(view, "item selected at "+position,null,null);
             }
         });
 
